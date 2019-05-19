66f070a05c27833b133b9a46d892d17014f5db0f
Kiana Nacua <kiananacua@Kianas-MacBook-Pro.local>
2019-05-18 22:46:35 -0700

    hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,106 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- *
- *  Copyright 2015 Adobe Systems Incorporated
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Adobe Systems Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Adobe Systems Incorporated and its
- * suppliers and are protected by all applicable intellectual property
- * laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe Systems Incorporated.
- **************************************************************************/
-define(['underscore', 'backbone'], function(_, Backbone) {
-  "use strict";
+/$(document).ready(function() {
 
-  return _.extend({}, Backbone.Events);
+  var wins = 0;
+  var lost = 0;
+  var score = 0;
+
+var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+$("#randomNumber").html(computerGuess);
+
+var stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+
+console.log("computerGuess" + computerGuess);
+console.log("stone1 " + stone1);
+console.log("stone2 " + stone2);
+console.log("stone3 " + stone3);
+console.log("stone4 " + stone4);
+
+function reset () {
+
+  score = 0;
+  $("#score").html(score);
+  computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+  $("#randomNumber").html(computerGuess);
+
+  stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
+  stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
+  stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
+  stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+
+}
+
+$("#stone1").on("click", function() {
+
+              score = score + stone1;
+              $("#score").html(score);
+              console.log("score1 " + score);
+              if (score == computerGuess) {
+                  checkifwon();
+              }
+              else if (score > computerGuess) {
+                  checkifwon();
+              }
+              
+});
+
+$("#stone2").on("click", function() {
+
+              score = score + stone2;
+              $("#score").html(score);
+              console.log("score2 " + score);
+              if (score == computerGuess) {
+              checkifwon();
+              }
+              else if (score > computerGuess) {
+              checkifwon();
+  }
 
 });
+
+$("#stone3").on("click", function() {
+
+              score = score + stone3;
+              $("#score").html(score);
+              console.log("score3 " + score);
+              if (score == computerGuess) {
+              checkifwon();
+              }
+              else if (score > computerGuess) {
+              checkifwon();
+}
+
+});
+
+$("#stone4").on("click", function() {
+
+              score = score + stone4;
+              $("#score").html(score);
+              console.log("score4 " + score);
+              if (score == computerGuess) {
+              checkifwon();
+              }
+              else if (score > computerGuess) {
+              checkifwon();
+}
+
+});
+
+function checkifwon() {
+
+  if(score === computerGuess) {
+
+  wins++;
+  console.log(wins);
+  $("#winOrLose").html("You Win!!");
+  $("#wins").html("Wins: " + wins);
+  reset();
+  }
+  else if (score > computerGuess) {
+
+  lost++;
+  console.log(lost);
+  $("#winOrLose").html("You Lost!!");
+  $("#losses").html("Losses: " + lost);
